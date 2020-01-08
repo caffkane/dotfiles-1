fe6d495a480d02ed8555d0dae52a12bd790c6d2f
Logan Kane <logan.kane@risepeople.com>
2020-01-08 15:01:08 -0800

    edited setup and zshrc_config
    I have edited setup.sh to symlink the files properly. I have also edited zshrc_config to be user agnostic for ease of use

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 
 mkdir -p ~/.config/nvim
 
-ln -s zshrc_config ~/.zshrc
+# Removes user old vim init and symlinks
+rm -rf ~/.config/nvim/init.vim 
+cp vim_config.vim ~/.config/nvim/init.vim
 ln -s vim_config.vim ~/.config/nvim/init.vim
+
+# Removes user old tmux conf and symlinks
+rm -rf ~/.tmux.conf
+cp tmux.conf ~/.tmux.conf
 ln -s tmux.conf ~/.tmux.conf
+
+# Removes user old zshrc and symlinks
+rm -rf ~/.zshrc
+cp zshrc_config ~/.zshrc
+ln -s zshrc_config ~/.zshrc
